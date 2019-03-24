d6f297e6d41ee1441372db534d2ee3d5afaf9613
JavierOlayo <JavierOlayo@Javier>
2019-03-24 15:31:54 -0600

    cambio de titulos

--- index.php
+++ index.php
@@ -38,12 +38,12 @@
 
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav ml-auto">
-	          <li class="nav-item active"><a href="index.html" class="nav-link">Principio</a></li>
-	          <li class="nav-item"><a href="about.html" class="nav-link">Sobre Nosotros</a></li>
+	          <li class="nav-item active"><a href="index.html" class="nav-link">Inivio</a></li>
+	          <li class="nav-item"><a href="about.html" class="nav-link">Acerca de Nosotros</a></li>
 	          <li class="nav-item"><a href="practice-areas.html" class="nav-link">Areas de Práctica</a></li>
 	          <li class="nav-item"><a href="attorneys.html" class="nav-link">Abogados</a></li>
 	          <li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>
-	          <li class="nav-item"><a href="contact.html" class="nav-link">Contacto</a></li>
+	          <li class="nav-item"><a href="contact.html" class="nav-link">Contáctanos</a></li>
 	        </ul>
 	      </div>
 	    </div>
